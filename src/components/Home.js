7f0433ffaaabedfe35cc65813e9fbdde3c0ee405
Niraj-123456 <lamaniraj321@gmail.com>
2021-05-20 21:41:07 +0545

    Add: header, icon with animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 function Home() {
     return (
         <Header>
             <p>Niraj Lama</p>
+            <i class="fas fa-arrow-circle-down"></i>
         </Header>
     )
 }
 
 export default Home
+
+const slideFromTop = keyframes`
+    from { transform: translateY(-100%); opacity: 0.3; }
+    to { transform: translateY(0%); opacity: 1; }
+`
 
 const Header = styled.div`
     color: white;
@@ -17,8 +23,14 @@ const Header = styled.div`
     text-align:center;
 
     p {
-        font-size: 25px;
+        font-size: 50px;
         font-weight: 600;
+        padding: 16px 28px;
+    }
+
+    i{
+        font-size: 70px;
+        animation: ${slideFromTop} ease-in 1s;
     }
 `
 
