f46f6b0dc3dbfe176a6028093258f5561de4593e
Niraj-123456 <lamaniraj321@gmail.com>
2021-05-21 19:36:04 +0545

    Add: hover effect to footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ function Footer() {
     return (
         <div>
             <FooterNav>
-                <p>&copy;copyright { date }</p>
+                <p>&copy; copyright { date }</p>
                 <Nav>
-                    <a href="#"><img src="../public/images/facebook_icon_130940.png" /></a>
-                    <a href="#"><img src="../public/images/instagram_icon.png" /></a>
-                    <a href="#"><img src="../public/images/github-logo.png" /></a>
+                    <a href="#"><img src="images/facebook_icon_130940.png" /></a>
+                    <a href="#"><img src="images/instagram_icon.png" /></a>
+                    <a href="#"><img src="images/github_icon_3.png" /></a>
                 </Nav>
             </FooterNav> 
         </div>
@@ -23,7 +23,7 @@ const FooterNav = styled.div`
     margin-top: 50px;
     height: 70px;
     width: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
+    background-color: rgba(0, 0, 0, 0.2);
     display: flex;
     align-items: center;
     color: #f9f9f9;
@@ -37,17 +37,23 @@ const Nav = styled.div`
     display: flex;
     flex: 1;
     justify-content: flex-end;
+    padding: 0 10px;
 
     a {
         text-decoration: none;
-        padding: 8px 16px;
+        padding: 8px 8px;
         color: #f9f9f9;
         cursor: pointer;
 
+        &:hover {
+            transform: scale(0.9, 0.9);
+        }
+
         img {
-            height: 20px;
-            width: 20px;
+        height: 30px;
+        width: 30px;
         }
     }
+}
     
 `
