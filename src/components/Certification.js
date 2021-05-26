b334181aa471c548af8bfe8f80a96b57f962556d
Niraj-123456 <lamaniraj321@gmail.com>
2021-05-26 17:38:58 +0545

    Add: certificate img

--- src/components/Certification.js
+++ src/components/Certification.js
@@ -6,12 +6,14 @@ function Certification() {
         <Container id="certification">
             <h2>Certifications</h2>
             <Content>
-                <p>Fugiat cupidatat in duis amet eu et. Nisi officia nulla sint laborum labore aliqua elit proident eu. 
-                Velit eu Lorem eu eu Lorem anim amet ullamco labore non ex eu. Qui ex cillum ad deserunt veniam. 
-                Cillum culpa ullamco duis cillum adipisicing anim fugiat pariatur. Velit do do velit deserunt fugiat 
-                nostrud aliquip nisi laborum officia sunt Lorem. Adipisicing culpa elit sint est eu id aute irure veniam 
-                deserunt do occaecat.</p>
+                <p>Here are all the certicates from the completion of different online courses. The certificate provider includes 
+                    Udemy, Cisco Acedemy, etc. The courses were 3 or more hours long and included basics concepts to advanced level 
+                    knowledge. The courses were really helpful in the upliftment of my knowledge and skills.
+                </p>
             </Content>
+            <Certificate>
+                <a href="https://drive.google.com/file/d/1Vb4wLxcWBh-MnBQtPPVGochWuIQ0w5yJ/view?usp=sharing" target="_blank"><img src="images/reactjs_certification_img.png"/></a>
+            </Certificate>
         </Container>
     )
 }
@@ -22,7 +24,6 @@ const Container = styled.div`
     width: 80%;
     margin: auto;
     text-align: center;
-    height: 500px;
 
     h2 {
         font-size: 30px;
@@ -41,4 +42,23 @@ const Content = styled.div`
         font-weight: 300;
     }
 
+`
+
+const Certificate = styled.div`
+    margin-top: 20px;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 20px;
+
+    img {
+        width: 80%;
+        height: 500px;
+        border-radius: 2px;
+
+        &:hover {
+            box-shadow: 0 5px 8px 8px rgba(0, 0, 0, 0.5);
+            transform: scale(1.01, 1.01);
+        }
+    }
 `
