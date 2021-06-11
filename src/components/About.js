12222f7242ffd0643f092c841ed39a91a8640807
Niraj-123456 <lamaniraj321@gmail.com>
2021-06-11 20:59:38 +0545

    contact form added

--- src/components/About.js
+++ src/components/About.js
@@ -12,11 +12,11 @@ function About() {
                 <p>My Name is Niraj Lama. I am a Front End React Js Developer looking for an internship
                     opportunity. I live in Boudha-6, Tinchuli, Kathmandu. I am an undergraduated student and did
                     my bachelors in 2018 from Shanker Dev Campus in Bachelors in Information Management. I did my
-                    +2 from St. Lawrence College and schooling from Gyanmala English High School.<br />
+                    +2 from St. Lawrence College and schooling from Gyanmala English High School.<br /><br />
                     Currently, I am working as a Digital Marketing Officer at Everest Soft Tech located in Basundhara,
                     Kathmandu. I always had the passion for Front End programming but did not get the change to start off 
                     my career in programming before. But I think it's never to late to start over. So, now I am teaching myself
-                    to code and looking for an opportunity to kick start my career in Front End Development. <br />
+                    to code and looking for an opportunity to kick start my career in Front End Development. <br /><br />
                     To sum up, I am a hard working guy and a problem solver.I have analytical thinking capabilities and good communicative 
                     skills. I consider my self a strong candidate not just of the technical skills but also non-technical skills 
                     that are as equally important as technical in any business firm. Thank You!
