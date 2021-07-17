b3acf204b638e43c41dbee3870cc91607c51e97c
Niraj-123456 <lamaniraj321@gmail.com>
2021-07-17 09:12:55 +0545

    added links to 2 more projects

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -1,70 +1,71 @@
 const projectDataArr = [
-    {
-      id: '1',
-      project: {
-        name: 'Basic JavaScript Project',
-        img: 'images/screenshot-niraj-123456.github.io-2021.05.26-20_47_53.png',
-        desc: 'This project is a basic javascript project which includes different apps such as rock, paper, scissor game, cat image generator using flex box, age convertor and lastly blackjack game.',
-        link: 'https://niraj-123456.github.io/nirajlama-javascriptproject.github.io/',
-        githubLink: 'https://github.com/Niraj-123456/nirajlama-javascriptproject.github.io.git',
-      },
-      completionDate: 'April 1, 2020'
+  {
+    id: "1",
+    project: {
+      name: "Basic JavaScript Project",
+      img: "images/screenshot-niraj-123456.github.io-2021.05.26-20_47_53.png",
+      desc: "This project is a basic javascript project which includes different apps such as rock, paper, scissor game, cat image generator using flex box, age convertor and lastly blackjack game.",
+      link: "https://niraj-123456.github.io/nirajlama-javascriptproject.github.io/",
+      githubLink:
+        "https://github.com/Niraj-123456/nirajlama-javascriptproject.github.io.git",
     },
-    {
-      id: '2',
-      project: {
-        name: 'Disney Plus Clone using Reactjs, Redux & Styled-Components',
-        img: 'https://previews.123rf.com/images/engkritchaya/engkritchaya1703/engkritchaya170300102/75165087-header-for-architect-concept-with-equipment-of-creative-project-background-architect-desk-.jpg',
-        desc: 'This project is a clone of disney plus app using reactjs, redux and styled components. This project includes firebase for a backend database for content and user login.', 
-        link: 'https://niraj-123456.github.io/disney-plus-clone/',
-        githubLink: 'https://github.com/Niraj-123456/disney-plus-clone.git',
-      },
-      completionDate: 'May 1, 2020'
+    completionDate: "April 1, 2020",
+  },
+  {
+    id: "2",
+    project: {
+      name: "Disney Plus Clone using Reactjs, Redux & Styled-Components",
+      img: "https://previews.123rf.com/images/engkritchaya/engkritchaya1703/engkritchaya170300102/75165087-header-for-architect-concept-with-equipment-of-creative-project-background-architect-desk-.jpg",
+      desc: "This project is a clone of disney plus app using reactjs, redux and styled components. This project includes firebase for a backend database for content and user login.",
+      link: "https://niraj-123456.github.io/disney-plus-clone/",
+      githubLink: "https://github.com/Niraj-123456/disney-plus-clone.git",
     },
-    {
-      id: '3',
-      project: {
-        name: 'React Project Diary',
-        img: 'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3978884.jpg',
-        desc: 'This React project includes different apps such as tic tac toe game, clock app, timer and blackjack game using react hooks.',
-        link: 'https://niraj-123456.github.io/nirajlama-javascriptproject.github.io/',
-        githubLink: 'https://github.com/Niraj-123456/reactjs-project-diary.git',
-      },
-      completionDate: 'June 1, 2020'
+    completionDate: "May 1, 2020",
+  },
+  {
+    id: "3",
+    project: {
+      name: "React Project Diary",
+      img: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3978884.jpg",
+      desc: "This React project includes different apps such as tic tac toe game, clock app, timer and blackjack game using react hooks.",
+      link: "https://niraj-123456.github.io/nirajlama-javascriptproject.github.io/",
+      githubLink: "https://github.com/Niraj-123456/reactjs-project-diary.git",
     },
-    {
-      id: '4',
-      project: {
-        name: 'Internship Challenge',
-        img: 'images/frontend_challenge.png',
-        desc: 'This project is a frontend challenge for an internship by younginnovations. This is a pure HTML5, CSS and JavaScript project with interactivity. It also has some animations and is fully responsive site.',
-        link: 'https://niraj-123456.github.io/yipl-frontend-challenge/',
-        githubLink: 'https://github.com/Niraj-123456/yipl-frontend-challenge.git',
-      }, 
-      completionDate: 'August 1, 2020'
+    completionDate: "June 1, 2020",
+  },
+  {
+    id: "4",
+    project: {
+      name: "Internship Challenge",
+      img: "images/frontend_challenge.png",
+      desc: "This project is a frontend challenge for an internship by younginnovations. This is a pure HTML5, CSS and JavaScript project with interactivity. It also has some animations and is fully responsive site.",
+      link: "https://niraj-123456.github.io/yipl-frontend-challenge/",
+      githubLink: "https://github.com/Niraj-123456/yipl-frontend-challenge.git",
     },
-    {
-      id: '5',
-      project: {
-        name: 'Weather App',
-        img: 'images/frontend_challenge.png',
-        desc: 'This project is a simple weather app for fetching weather infos from a free api source called openweathermap.org. It takes city name as an input and displays current weather info of the city.',
-        link: '',
-        githubLink: '',
-      }, 
-      completionDate: 'March 25, 2021'
+    completionDate: "August 1, 2020",
+  },
+  {
+    id: "5",
+    project: {
+      name: "Weather App",
+      img: "images/frontend_challenge.png",
+      desc: "This project is a simple weather app for fetching weather infos from a free api source called openweathermap.org. It takes city name as an input and displays current weather info of the city.",
+      link: "",
+      githubLink: "",
     },
-    {
-      id: '6',
-      project: {
-        name: 'Social Media App',
-        img: 'images/frontend_challenge.png',
-        desc: 'This project is my version of social media app which has an auth system for user management. After user registration and login, user can perform CRUD operation. This app is still work in progress.',
-        link: '',
-        githubLink: '',
-      }, 
-      completionDate: 'June 1, 2021'
-    }
-  ]
+    completionDate: "March 25, 2021",
+  },
+  {
+    id: "6",
+    project: {
+      name: "Social Media App",
+      img: "images/social_media_app_screenshot.jpg",
+      desc: "This project is my version of social media app which has an auth system for user management. After user registration and login, user can perform CRUD operation. This app is still work in progress.",
+      link: "https://niraj-123456.github.io/my-socialmedia-app/",
+      githubLink: "",
+    },
+    completionDate: "June 1, 2021",
+  },
+];
 
-  export default projectDataArr
+export default projectDataArr;
